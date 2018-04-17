2df7230f006ba8c2f52c478742170e735158f43c
fedy <fedy.salah@gmail.com>
2018-04-17 15:28:52 +0200

    add some pwa requirements

--- src/main.js
+++ src/main.js
@@ -30,18 +30,6 @@ const config = {
 
 firebase.initializeApp(config);
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
-
 navigator.vibrate = navigator.vibrate ||
     navigator.webkitVibrate ||
     navigator.mozVibrate ||
